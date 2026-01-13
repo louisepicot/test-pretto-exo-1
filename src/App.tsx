f8c6e13654555c04594c27deaaafd76326e226c0
Louise Picot <louise.picot@needelp.com>
2026-01-13 16:56:53 +0100

    feat: integrate new components into app structure
    - Update App.tsx to use new layout and section components
    - Update main.tsx to use path aliases
    - Add vite-env.d.ts for TypeScript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import './App.css'
+import "@/index.css";
+import { Container } from "@/components/layout/Container";
+import { PageBackground } from "@/components/layout/PageBackground";
+import { CreditSimulationSection } from "@/components/sections/CreditSimulationSection";
+import { Section } from "@/components/sections/Section";
+import { FinancingCertificateSection } from "@/components/sections/FinancingCertificateSection";
+import { Header } from "@/components/layout/Header";
+
 
 function App() {
-  return <div />
+  return (
+    <>
+      <Header />
+      <PageBackground>
+        <Container>
+          <Section>
+            <CreditSimulationSection />
+          </Section>
+          <Section>
+            <FinancingCertificateSection />
+          </Section>
+        </Container>
+      </PageBackground>
+    </>
+  );
 }
 
-export default App
+export default App;
